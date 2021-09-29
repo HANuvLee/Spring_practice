f95904c243ce955157166860aa5f06c4aa959e6f
HANuvLee <ejh9268@naver.com>
2021-09-29 23:19:03 +0900

    maven jpa

---
+++ JPA/practiceJPA/src/test/java/com/lee/practiceJPA/PracticeJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.practiceJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
